23309c2ba0ca0dcab4aa937a2ec65d45456c3324
Sajid <osmansazid13@gmail.com>
2022-06-14 00:37:41 +0600

    Added Spring MVC for Book

--- src/main/java/sajid/springframework/webApp/domain/Author.java
+++ src/main/java/sajid/springframework/webApp/domain/Author.java
@@ -75,7 +75,6 @@ public class Author {
                 "Id='" + id + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
--- src/main/java/sajid/springframework/webApp/domain/Publisher.java
+++ src/main/java/sajid/springframework/webApp/domain/Publisher.java
@@ -99,7 +99,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
